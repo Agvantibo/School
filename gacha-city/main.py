80f5589d34cbc8f85e751cc8196a694de0bad80f
agvantibo <derez08@yandex.ru>
2021-03-23 22:16:15 +0300

    ~

--- gacha-city/main.py
+++ gacha-city/main.py
@@ -1,5 +1,41 @@
 #!/bin/pypy3
 from tkinter import *
-from random import *
-from intersection import *
+from PIL import Image
+from PIL import ImageTk
 
+root = Tk()
+w, h = 1474, 1056
+cnv = Canvas(root, width=w, height=h)
+
+root.title("It is totally not suspicious")
+root.geometry(str(h) + "x" + str(w))
+root.configure(background="black")
+
+
+class Example(Frame):
+    def __init__(self, master, *pargs):
+        Frame.__init__(self, master, *pargs)
+
+        self.image = Image.open('./assets/creepy/stop/stop_it/okay/informatics.png')
+        self.img_copy= self.image.copy()
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+
+        self.background = Label(self, image=self.background_image)
+        self.background.pack(fill=BOTH, expand=YES)
+        self.background.bind('<Configure>', self._resize_image)
+
+    def _resize_image(self, event):
+
+        new_width = event.width
+        new_height = event.height
+
+        self.image = self.img_copy.resize((new_width, new_height))
+
+        self.background_image = ImageTk.PhotoImage(self.image)
+        self.background.configure(image =  self.background_image)
+
+
+e = Example(root)
+e.pack(fill=BOTH, expand=YES)
+mainloop()
